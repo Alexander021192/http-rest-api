ef2560d931b01f9122beff4cfd23c24acbfcd8a9
alsavchenko <alsavchenko@ozon.ru>
2022-06-29 07:41:36 +0300

    add validations

--- db/migrations/000001_create_users.down.sql
+++ db/migrations/000001_create_users.down.sql
@@ -1,5 +1,1 @@
-create table users (
-    id bigserial noy null primary key,
-    email varchar not null unique,
-    encrypted_password varchar not null
-);
+DROP TABLE users;
--- db/migrations/000001_create_users.up.sql
+++ db/migrations/000001_create_users.up.sql
@@ -1,1 +1,5 @@
-drop table users;
+CREATE TABLE users (
+    id bigserial not null primary key,
+    email varchar not null unique,
+    encrypted_password varchar not null
+);
